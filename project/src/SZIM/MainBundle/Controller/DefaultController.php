685be7c0a0ee048d3091d6f121ed18eb2193b58f
nissyre <nissyre@gmail.com>
2014-09-15 21:58:32 +0200

    Refactoring demodata

--- project/src/SZIM/MainBundle/Controller/DefaultController.php
+++ project/src/SZIM/MainBundle/Controller/DefaultController.php
@@ -1,10 +1,18 @@
 <?php
 
+/**
+ * Entiti class represent ShortCode record
+ *
+ * @author Mateusz Antkowiak
+ * @version 0.0.3
+ */
+
 namespace SZIM\MainBundle\Controller;
 
 use SZIM\MainBundle\Entity\Page;
 use SZIM\MainBundle\Entity\ShortCode;
 use SZIM\MainBundle\Models\SZIMPage;
+use SZIM\MainBundle\Models\SZIMDemoData;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -18,74 +26,13 @@ class DefaultController extends Controller {
         return $this->render('SZIMMainBundle:Default:index.html.twig', array('content' => $content));
     }
 
-    public function testdata_plainpagesAction() {
-
-        $em = $this->getDoctrine()->getManager();
-
-        $page = new Page();
-        $page->setName('test1');
-        $page->setVersion('0.0.1');
-        $page->setStatus('deployed');
-        $page->setContent('<h1> Strona testowa1 </h1>');
-
-        $em->persist($page);
-        $em->flush();
-
-        $page = new Page();
-        $page->setName('test2');
-        $page->setVersion('0.0.1');
-        $page->setStatus('deployed');
-        $page->setContent('<h1> Strona testowa2 </h1>');
-
-        $em->persist($page);
-        $em->flush();
-
-        $page = new Page();
-        $page->setName('index');
-        $page->setVersion('0.0.1');
-        $page->setStatus('deployed');
-        $page->setContent('<h1> Strona główna </h1>');
-
-        $em->persist($page);
-        $em->flush();
-
-        return 1;
-    }
-
-    public function testdata_pageswithshortcodesAction() {
-        $em = $this->getDoctrine()->getManager();
-
-        $page = new Page();
-        $page->setName('testshortcodes1');
-        $page->setVersion('0.0.1');
-        $page->setStatus('deployed');
-        $page->setContent('<h1> Strona testowa ShortCode 1  </h1> <br /> [Program]');
-
-        $em->persist($page);
-        $em->flush();
-
-        $page = new Page();
-        $page->setName('testshortcodes2');
-        $page->setVersion('0.0.1');
-        $page->setStatus('deployed');
-        $page->setContent('<h1> Strona testowa ShortCode 2  </h1> <br/> [Program]<br />[Program]<br />[Program]');
-
-        $em->persist($page);
-        $em->flush();
-    }
-
-    public function testdata_shortcodesAction() {
-        $em = $this->getDoctrine()->getManager();
-
-        $shortcode = new ShortCode();
-        $shortcode->setName('PROGRAM');
-        $shortcode->setBundle('Program');
-        $shortcode->setAction('index');
-        $shortcode->setVersion('0.0.1');
-        $shortcode->setPackage('SZIM');
-        $shortcode->setControllerName('Default');
-        $em->persist($shortcode);
-        $em->flush();
+    public function testdataAction() {
+        // $this->getDoctrine()->getManager();
+        $demodate = new SZIMDemoData($this);
+        $result = $demodate->install_demo_data();
+        $content = 'Nie udało się zainstalować danych demo.';
+        if ($result == 1) {$content = 'Poprawnie zainstalowano dane demo.';}
+        return $this->render('SZIMMainBundle:Default:demodata.html.twig', array('content' => $content));;
     }
 
 }
--- project/src/SZIM/MainBundle/Models/SZIMDemoData.php
+++ project/src/SZIM/MainBundle/Models/SZIMDemoData.php
@@ -7,7 +7,86 @@
  * @version 0.0.1
  */
 
-class SZIMPage {
+namespace SZIM\MainBundle\Models;
+use SZIM\MainBundle\Entity\Page;
+use SZIM\MainBundle\Entity\ShortCode;
+
+class SZIMDemoData {
+	private $controller;
+
+	public function __construct($controller) {
+		$this->controller = $controller;
+	}
+
+	public function install_demo_data() {
+		$this->install_pages();
+		$this->install_shortcodes();
+
+		return true;
+	}
+
+	private function install_pages() {
+        $em = $this->controller->getDoctrine()->getManager();
+
+        $page = new Page();
+        $page->setName('test1');
+        $page->setVersion('0.0.1');
+        $page->setStatus('deployed');
+        $page->setContent('<h1> Strona testowa1 </h1>');
+
+        $em->persist($page);
+        $em->flush();
+
+        $page = new Page();
+        $page->setName('test2');
+        $page->setVersion('0.0.1');
+        $page->setStatus('deployed');
+        $page->setContent('<h1> Strona testowa2 </h1>');
+
+        $em->persist($page);
+        $em->flush();
+
+        $page = new Page();
+        $page->setName('index');
+        $page->setVersion('0.0.1');
+        $page->setStatus('deployed');
+        $page->setContent('<h1> Strona główna </h1>');
+
+        $em->persist($page);
+        $em->flush();
+
+        $page = new Page();
+        $page->setName('testshortcodes1');
+        $page->setVersion('0.0.1');
+        $page->setStatus('deployed');
+        $page->setContent('<h1> Strona testowa ShortCode 1  </h1> <br /> [Program]');
+
+        $em->persist($page);
+        $em->flush();
+
+        $page = new Page();
+        $page->setName('testshortcodes2');
+        $page->setVersion('0.0.1');
+        $page->setStatus('deployed');
+        $page->setContent('<h1> Strona testowa ShortCode 2  </h1> <br/> [Program]<br />[Program]<br />[Program]');
+
+        $em->persist($page);
+        $em->flush();
+	}
+
+	private function install_shortcodes() {
+		$em = $this->controller->getDoctrine()->getManager();
+
+        $shortcode = new ShortCode();
+        $shortcode->setName('PROGRAM');
+        $shortcode->setBundle('Program');
+        $shortcode->setAction('index');
+        $shortcode->setVersion('0.0.1');
+        $shortcode->setPackage('SZIM');
+        $shortcode->setControllerName('Default');
+        $em->persist($shortcode);
+        $em->flush();
+	}
 
 
 
